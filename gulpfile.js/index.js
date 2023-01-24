20d9f0a529c9859d968ffe4c83fa19ff1276658b
Ksenny <ksenny_h@mail.ru>
2023-01-24 21:51:31 +0300

    header pug

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -8,7 +8,7 @@ const app = require("./config/app");
 // Задачи
 
 const clear = require('./task/clear.js');
-const html = require('./task/html.js');
+const pug = require('./task/pug.js');
 const scss = require('./task/scss.js');
 const js = require('./task/js.js');
 const img = require('./task/img.js');
@@ -26,7 +26,7 @@ const server = () => {
 
 
 const watcher = () => {
-    watch(path.html.watch, html).on('all', browserSync.reload);
+    watch(path.pug.watch, pug).on('all', browserSync.reload);
     watch(path.scss.watch, scss).on('all', browserSync.reload);
     watch(path.js.watch, js).on('all', browserSync.reload);
     watch(path.img.watch, img).on('all', browserSync.reload);
@@ -35,7 +35,7 @@ const watcher = () => {
 
 const build = series(
     clear,
-    parallel(html, scss, js, img, font, plagins)
+    parallel(pug, scss, js, img, font, plagins)
 );
 
 const dev = series(
@@ -44,7 +44,7 @@ const dev = series(
 );
 
 
-exports.html = html;
+exports.pug = pug;
 exports.watch = watcher;
 exports.clear = clear;
 exports.scss = scss;
